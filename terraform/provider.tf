bcce12063f8af7a179fbc297bce492f99e027ce4
Bertrand Cachet <bertrand.cachet@gmail.com>
2025-04-04 09:58:27 +0000

    devcontainer: rely more on features instead of customizing container file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,6 +8,6 @@ terraform {
 }
 
 provider "exoscale" {
-  key = "${var.exoscale_api_key}"
-  secret = "${var.exoscale_secret_key}"
+  key    = var.exoscale_api_key
+  secret = var.exoscale_secret_key
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,30 +9,30 @@ variable "exoscale_secret_key" {
 }
 
 variable "kube_version" {
-    description = "Version of the Kubernetes cluster"
-    type        = string
-    default     = ""
-    # when default is an empty string, the latest kubernetes version available is used
+  description = "Version of the Kubernetes cluster"
+  type        = string
+  default     = ""
+  # when default is an empty string, the latest kubernetes version available is used
 }
 
 variable "name" {
-    description = "Name of the cluster"
-    type        = string
-    default     = "actions-runner"
+  description = "Name of the cluster"
+  type        = string
+  default     = "actions-runner"
 }
 
 variable "workers_number" {
-    description = "Number of workers in node pool"
-    type        = number
-    default     = 3
+  description = "Number of workers in node pool"
+  type        = number
+  default     = 3
 }
 
 variable "worker_type" {
-    type    = string
-    default = "standard.medium"
+  type    = string
+  default = "standard.medium"
 }
 
 variable "zone" {
-    type    = string
-    default = "ch-zrh-1"
+  type    = string
+  default = "ch-dk-2"
 }
