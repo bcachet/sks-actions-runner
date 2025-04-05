23164f6634019c7ebedc429b5a48e7cce6aed587
Bertrand Cachet <bertrand.cachet@gmail.com>
2025-04-05 22:40:44 +0000

    helm provider to install Github actions-runner-controller

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,3 +11,9 @@ provider "exoscale" {
   key    = var.exoscale_api_key
   secret = var.exoscale_secret_key
 }
+
+provider "helm" {
+  kubernetes {
+    config_path = local_sensitive_file.sks_kubeconfig_file.filename
+  }
+}
